66e7e74019e6bb67dad1b26356bdd35042409df8
Lim, Chee Kin <chee-kin.lim@dxc.com>
2019-06-11 16:30:38 +0800

    ported the rasa-workshop-pydata-dc project from notebook to code

--- actions.py
+++ actions.py
@@ -1,27 +1,21 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+import requests
 
+class ApiAction(Action):
+    def name(self) -> Text:
+        return "action_paper_search"
 
-# This is a simple example for a custom action which utters "Hello World!"
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        paper_type = tracker.get_slot('paper_type')
+        
+        response = requests.get('http://dblp.org/search/publ/api?q={}&format=json&h=1'.format(paper_type)).json()
+        title = response['result']['hits']['hit'][0]['info']['title']
+        authors = response['result']['hits']['hit'][0]['info']['authors']['author'][0]
+        link = response['result']['hits']['hit'][0]['info']['url']
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message("Hello World!")
-#
-#         return []
+        dispatcher.utter_message('I found a paper called {}.'.format(title))
+        return [SlotSet("link", link), SlotSet("authors", authors)]
